cb36fc3028e19025244f97f47f940d9f05348bd4
Matheus <math.gsilva07@gmail.com>
2024-09-08 20:43:48 +0000

    update code

--- connetion/script/theme.js
+++ connetion/script/theme.js
@@ -1,15 +1,15 @@
 function toggleTheme() {
-  const root = document.documentElement;
-  const themeIcon = document.getElementById("themeIcon");
+  const root = document.documentElement
+  const themeIcon = document.getElementById("themeIcon")
 
   if (root.classList.contains("light")) {
-    root.classList.remove("light");
-    root.classList.add("dark");
-    themeIcon.textContent = "toggle_on";
+    root.classList.remove("light")
+    root.classList.add("dark")
+    themeIcon.textContent = "toggle_on"
   } else {
-    root.classList.remove("dark");
-    root.classList.add("light");
-    themeIcon.textContent = "toggle_off";
+    root.classList.remove("dark")
+    root.classList.add("light")
+    themeIcon.textContent = "toggle_off"
   }
 }
 
@@ -17,16 +17,16 @@ document.getElementById("theme").addEventListener("click", toggleTheme);
 
 window.onload = () => {
   const root = document.documentElement;
-  const themeIcon = document.getElementById("themeIcon");
+  const themeIcon = document.getElementById("themeIcon")
 
   if (
     window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
+    window.matchMedia("(prefers-color-scheme: light)").matches
   ) {
-    root.classList.add("dark");
-    themeIcon.textContent = "toggle_on";
+    root.classList.add("dark")
+    themeIcon.textContent = "toggle_on"
   } else {
-    root.classList.add("light");
-    themeIcon.textContent = "toggle_off";
+    root.classList.add("light")
+    themeIcon.textContent = "toggle_off"
   }
-};
+}
--- connetion/script/timer.js
+++ connetion/script/timer.js
@@ -1,40 +1,49 @@
-let startTime;
-let updatedTime;
-let difference;
-let tInterval;
-let running = false;
+let workTime = 25 * 60 * 1000;
+let breakTime = 5 * 60 * 1000; 
+let isWorkSession = true;
+let timeLeft = workTime
+let tInterval
+let running = false
 let timerDisplay = document.getElementById('timer');
+let controlButton = document.getElementById('controlButton')
 
-function startTimer() {
+function controlTimer() {
     if (!running) {
-        startTime = new Date().getTime();
-        tInterval = setInterval(updateTimer, 1);
-        running = true;
+        startTimer()
+    } else {
+        pauseTimer()
     }
 }
 
+function startTimer() {
+    tInterval = setInterval(updateTimer, 1000)
+    running = true
+    controlButton.innerHTML = "Parar"
+}
+
 function updateTimer() {
-    updatedTime = new Date().getTime();
-    difference = updatedTime - startTime;
+    timeLeft -= 1000
 
-    let hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
-    let minutes = Math.floor((difference / (1000 * 60)) % 60);
-    let seconds = Math.floor((difference / 1000) % 60);
+    let minutes = Math.floor((timeLeft / (1000 * 60)) % 60)
+    let seconds = Math.floor((timeLeft / 1000) % 60)
 
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    minutes = (minutes < 10) ? "0" + minutes : minutes
+    seconds = (seconds < 10) ? "0" + seconds : seconds
 
-    timerDisplay.innerHTML = hours + ":" + minutes + ":" + seconds;
+    timerDisplay.innerHTML = minutes + ":" + seconds
+
+    if (timeLeft <= 0) {
+        clearInterval(tInterval)
+        running = false
+        isWorkSession = !isWorkSession;
+        timeLeft = isWorkSession ? workTime : breakTime
+        controlButton.innerHTML = "Inciar"
+        alert(isWorkSession ? "Hora de trabalhar!" : "Hora de descansar!")
+    }
 }
 
 function pauseTimer() {
-    clearInterval(tInterval);
+    clearInterval(tInterval)
     running = false;
-}
-
-function resetTimer() {
-    clearInterval(tInterval);
-    running = false;
-    timerDisplay.innerHTML = "00:00:00";
+    controlButton.innerHTML = "Continuar"
 }
