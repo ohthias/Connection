61bb9fbd74ea82b24efc83e27ec7dfb1bcdcac51
Matheus Gabriel <math.gsilva07@gmail.com>
2025-04-15 11:20:42 -0300

    update files

--- connection/main.js
+++ connection/main.js
@@ -1,9 +1,14 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600
+        width: 250,
+        height: 330,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+          },
+        icon: path.join(__dirname, 'favicon_io', 'favicon-32x32'),
     })
 
     win.loadFile('index.html')
